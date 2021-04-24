2cceef89d5a28ec403f41ed7fa24d87ec77636c1
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2021-04-24 22:41:44 +0200

    adds swagger

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,26 @@ import Fastify from 'fastify';
 import SimpleRoutes from './routes/simple';
 import TokenRoutes from './routes/token';
 import FooPlugin from './connectors/FooPlugin';
+import fastifySwagger from 'fastify-swagger';
 
 const fastify = Fastify({
   logger: true
+})
+
+fastify.register(fastifySwagger, {
+  routePrefix: '/docs',
+  exposeRoute: true,
+  openapi: {
+    info: {
+      title: 'Test swagger',
+      description: 'testing the fastify swagger api',
+      version: '0.1.0'
+    },
+  },
+  uiConfig: {
+    docExpansion: 'full',
+    deepLinking: false
+  },
 })
 
 fastify.register(FooPlugin, {
